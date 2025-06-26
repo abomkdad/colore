84a41803ea18ff3d7e0e207ae772f2b1af916d90
abomkdad <abomkdad2@gmail.com>
2025-06-26 05:20:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ fetch(sheetUrl)
       const code = row.c[0]?.v || '';
       const img = row.c[2]?.v || '';
       const tags = [];
-if (row.c[4]?.v?.toString().trim() === '1') tags.push('מרתק');
+if (row.c[4]?.v?.toString().trim() === '3') tags.push('מרתק');
 if (row.c[5]?.v?.toString().trim() === '2') tags.push('עורועץ');
 if (row.c[6]?.v?.toString().trim() === '1') tags.push('חמוץ');
 if (row.c[7]?.v?.toString().trim() === '1') tags.push('מרענן');
